859c7267dc66549f82d83d57d8998ec0695e10b2
Gonzalo Pereyra <gonzapereyra93@gmail.com>
2025-03-31 17:17:27 -0300

    Agregue una rama ejercicio2 y un archivo ejercicio 2

---
+++ ejercicio2.py
@@ -0,0 +1,4 @@
+nombre=input("ingrese su nombre")
+apellido= input("Ingrese su apellido")
+materia= "programacion"
+print(f"{nombre} {apellido} es estudiante de {materia}")
---
+++ prueba.py
@@ -0,0 +1,3 @@
+rutaarchivo = "kasd]\nd"
+
+print(rutaarchivo)
